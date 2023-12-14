8f7c0816989ec6ce7fd6ed3f22f81accbd1e4c8c
irene <irenejim9504@gmail.com>
2023-12-14 22:17:49 +0300

    completed my arithmetic assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1=31;
+let num2=2;
 
-// Write your code here
+let multiply =num1* num2;
+
+console.log(multiply);
+
+
+let random = 0;
+random++;
+
+
+let num3 = 14;
+let num4 = 5;
+
+
+let mod =num3 % num4;
+console.log(mod);
+
+
+let max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+console.log(math);
